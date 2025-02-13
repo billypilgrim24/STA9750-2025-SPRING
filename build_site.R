eaccd51fcf87e0f1ca9593038a06faf19e5817c8
Charles Wojcik <cwojcik24@gmail.com>
2025-02-13 03:17:36 -0500

    update

--- build_site.R
+++ build_site.R
@@ -7,5 +7,5 @@ if(!quarto::quarto_binary_sitrep()){
   stop("Something is wrong with your quarto installation.")
 }
 quarto::quarto_render(".")
-system("git add 'docs/Charles_Wojcik_Resume.pdf'")
+system("git add docs/Charles_Wojcik_Resume.pdf")
 if(!any(grepl("rstudio", search()))){q("no")}
